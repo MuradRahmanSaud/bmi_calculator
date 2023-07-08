d1c719756c3e7a23ecb4703656040c41acf079d9
MuradRahmanSaud <murad.rahman.saud@gmail.com>
2023-07-08 10:48:50 +0600

    V2

---
+++ lib/home_Page.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Color bgColor = Color(0xff0E0E0E);
+  Color cardColor = Color(0xff28293D);
+  bool isMale = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: bgColor,
+      appBar: AppBar(
+        title: Text('BMI Calculator Murad'),
+        backgroundColor: bgColor,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 10,
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 5,
+                  child: Card(
+                    color: isMale == true ? Colors.teal : cardColor,
+                    child: InkWell(
+                      onTap: () {
+                        setState(() {
+                          isMale = true;
+                        });
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.male,
+                            color: Colors.white,
+                            size: 45,
+                          ),
+                          Text(
+                            'Male',
+                            style: TextStyle(color: Colors.white, fontSize: 25),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 5,
+                  child: Card(
+                    color: isMale == false ? Colors.teal : cardColor,
+                    child: InkWell(
+                      onTap: () {
+                        setState(() {
+                          isMale = false;
+                        });
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.male,
+                            color: Colors.white,
+                            size: 45,
+                          ),
+                          Text(
+                            'Male',
+                            style: TextStyle(color: Colors.white, fontSize: 25),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 10,
+            child: Container(
+              color: Colors.amber,
+            ),
+          ),
+          Expanded(
+            flex: 10,
+            child: Container(
+              color: Colors.red,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
